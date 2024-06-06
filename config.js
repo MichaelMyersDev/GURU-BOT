c0b5a8ad8b0b6bef3d2803a9fe344d0bdc88353d
MichaelMyersDev <168398453+MichaelMyersDev@users.noreply.github.com>
2024-06-05 21:38:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['923551211906', 'me', true],
+  ['923551211906', 'Ash', true], 
   [''] 
 ] //Number of owners
 
